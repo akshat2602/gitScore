0c1ee9a2e306a5eaa5aec18e52612270f34c81ef
Akshat Sharma <akshatsharma2602@gmail.com>
2022-01-29 23:58:37 +0530

    Added custom themes

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
-import { ChakraProvider } from '@chakra-ui/react'
+import { ChakraProvider } from "@chakra-ui/react";
 import type { AppProps } from "next/app";
+import { customTheme } from "../themes/index";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
+    <ChakraProvider theme={customTheme}>
       <Component {...pageProps} />
     </ChakraProvider>
-  )
+  );
 }
 
-export default MyApp
+export default MyApp;
